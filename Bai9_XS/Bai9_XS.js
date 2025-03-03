00a0ee60a404dec74fe9aeaf408702fb7f7c9ced
Pham-anh-Hub <phamphuonganhcutee@gmail.com>
2025-03-03 08:03:41 +0700

    update Bai9 ss18

--- Bai9_XS/Bai9_XS.js
+++ Bai9_XS/Bai9_XS.js
@@ -2,23 +2,27 @@ let hour = +prompt(`Nhập vào giờ`);
 let minute = +prompt(`Nhập vào phút`);
 let second = +prompt(`Nhập vào giây`);
 
-let hourUnit;
+if((hour>=0 && hour<=24) && (minute>=0 && minute<=60) && (second>=0 && second<=60) ){
+    let hourUnit;
+    if(hour >= 0 && hour <= 12){
+        hourUnit = "AM";
+        hour = hour % 12;
+        console.log(`${hour}:${minute}:${second} ${hourUnit}`);    
+    }else if(hour >= 12 && hour <= 24){
+        hourUnit = "PM";
+        hour = hour % 12;
+        console.log(`${hour}:${minute}:${second} ${hourUnit}`);    
+    } 
+}else{
+    alert(`Thời gian nhập vào chưa hợp lệ`);
+}
+
 
 // if(hour >= 0 && hour <= 12){
 //     hourUnit = "AM";
-//     hour = hour % 12;
-//     console.log(`${hour}:${minute}:${second} ${hourUnit}`);    
-// }else if(hour >= 12 && hour <= 24){
+//     console.log(`${hour}:${minute}:${second} ${hourUnit}`)
+// }else if(hour > 12 && hour <= 24){
 //     hourUnit = "PM";
-//     hour = hour % 12;
-//     console.log(`${hour}:${minute}:${second} ${hourUnit}`);    
+//     hour = hour - 12;
+//     console.log(`${hour}:${minute}:${second} ${hourUnit}`)
 // }
-
-if(hour >= 0 && hour <= 12){
-    hourUnit = "AM";
-    console.log(`${hour}:${minute}:${second} ${hourUnit}`)
-}else if(hour > 12 && hour <= 24){
-    hourUnit = "PM";
-    hour = hour - 12;
-    console.log(`${hour}:${minute}:${second} ${hourUnit}`)
-}
